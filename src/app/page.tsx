60171b6f4791a0c830bd7764f530e2f8731b272e
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2024-05-29 09:57:25 +0900

    Chore: Add Rooms directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import Header from '@/components/Header/Header'
-import Footer from '@/components/Footer/Footer'
 export default function Home() {
-  return (
-    <>
-      <Footer />
-      {/* <Header /> */}
-    </>
-  )
+  return <></>
 }
---
+++ src/app/rooms/layout.jsx
@@ -0,0 +1,7 @@
+export default function RoomsLayout({ children }) {
+  return (
+    <html lang='ko'>
+      <body>{children}</body>
+    </html>
+  )
+}
