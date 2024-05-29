1c503e68fa3a3d7a3eacbdfacda05c82f2fb47a4
IforCU <tmdgus0844@gmail.com>
2024-05-29 14:19:29 +0900

    Feat: comment 컴포넌트 추가

---
+++ src/components/comment/Comment.jsx
@@ -0,0 +1,51 @@
+import GuestFavorite from '@/components/GuestFavorite/GuestFavorite'
+import ReviewMeta from '@/components/ReviewMeta/ReviewMeta'
+import Review from '@/components/Review/Review'
+import ReviewInform from '@/components/ReviewInform/ReviewInform'
+import GenerateReviewMetaData from '@/utils/GenerateReviewMetaData'
+import { useState, useEffect } from 'react'
+
+const Comment = () => {
+  const [reviewData, setReviewData] = useState([])
+  const [reviewMetaData, setReviewMetaData] = useState([])
+
+  useEffect(() => {
+    fetch('/api/review')
+      .then(response => response.json())
+      .then(data => {
+        setReviewData(processReviewData(data))
+        // @ts-ignore
+        setReviewMetaData(GenerateReviewMetaData(data))
+      })
+      .catch(error => console.error('Error fetching ReviewData.json:', error))
+  }, [])
+
+  // ratings 평균 값을 내주는 함수
+  const calculateAverageRating = ratings => {
+    const totalScore = Object.values(ratings).reduce((acc, curr) => acc + curr, 0)
+    const numberOfCategories = Object.keys(ratings).length
+    return (totalScore / numberOfCategories).toFixed(1)
+  }
+
+  // rating 객체로 가공하는 함수.
+  const processReviewData = data => {
+    return data.map(item => {
+      const averageRating = calculateAverageRating(item.Ratings)
+      return {
+        ...item.review,
+        rating: parseFloat(averageRating), // 소수점 한 자리까지 표시
+      }
+    })
+  }
+
+  return (
+    <div className='flex flex-col px-10'>
+      <GuestFavorite />
+      <ReviewMeta reviewMetaData={reviewMetaData} />
+      <Review reviewData={reviewData} reviewMetaData={reviewMetaData} />
+      <ReviewInform />
+    </div>
+  )
+}
+
+export default Comment
