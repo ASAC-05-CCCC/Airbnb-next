193be88f4755d1a133a6bf0807ae72264c2dfee9
IforCU <tmdgus0844@gmail.com>
2024-05-29 14:22:41 +0900

    Fix: merge 준비

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-'use client'
-import Host from '@/components/Host/Host'
-import Notification from '@/components/Notification/Notification'
-import Comment from '@/components/comment/Comment'
-
 export default function Home() {
-  return (
-    <>
-      <Comment />
-      <Host />
-      <Notification />
-    </>
-  )
+  return <></>
 }
