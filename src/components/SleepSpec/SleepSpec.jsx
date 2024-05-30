b7612e8d8e72a68d0b89d2c4b97453804d77030b
yunhyungNAM <iys04003@naver.com>
2024-05-30 13:23:43 +0900

    Refactor: change SleepSpec direction and image tag

--- src/components/SleepSpec/SleepSpec.jsx
+++ src/components/SleepSpec/SleepSpec.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import queenBed from '../../assets/SleepSpecSvg/queenBed.svg'
-import singleBed from '../../assets/SleepSpecSvg/singleBed.svg'
+import Image from 'next/image'
 
 const SleepSpec = () => {
   return (
@@ -11,8 +10,8 @@ const SleepSpec = () => {
           <div className="w-full sm:w-1/2 lg:w-1/4 px-2">
             <div className="border p-4 rounded-lg flex flex-col items-left justify-center aspect-square">
               <div className='flex items-center mb-4'>
-                <img src={queenBed} alt="Queen Bed" className='w-6 mr-2' />
-                <img src={queenBed} alt="Queen Bed" className='w-6' />
+                <Image src="/images/singleBed.svg" alt="Queen Bed" width={24} height={24} className='mr-2' />
+                <Image src="/images/queenBed.svg" alt="Queen Bed" width={24} height={24} />
               </div>
               <h3 className="text-xl font-semibold">침실 1</h3>
               <p>퀸사이즈 침대 2개</p>
@@ -21,8 +20,8 @@ const SleepSpec = () => {
           <div className="w-full sm:w-1/2 lg:w-1/4 px-2 mb-8">
             <div className="border p-4 rounded-lg flex flex-col items-left justify-center aspect-square">
               <div className='flex items-center mb-4'>
-                <img src={queenBed} alt="Queen Bed" className='w-6 mr-2' />
-                <img src={singleBed} alt="Single Bed" className='w-6' />
+                <Image src="/images/singleBed.svg" alt="Queen Bed" width={24} height={24}className='mr-2' />
+                <Image src="/images/queenBed.svg" alt="Queen Bed" width={24} height={24} />
               </div>
               <h3 className="text-xl font-semibold">침실 2</h3>
               <p>퀸사이즈 침대 1개,</p>
