662f7b33398cca34c5a36edcf79b8db54648608e
jueun0725 <kimjueun010@naver.com>
2024-06-02 21:55:17 +0900

    Feat: 좋아요 클릭시 하트 색깔 변경

--- src/components/PlaceList/PlaceCardImageList.jsx
+++ src/components/PlaceList/PlaceCardImageList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Pagination, Navigation } from 'swiper/modules'
 import 'swiper/css'
@@ -6,13 +6,20 @@ import 'swiper/css/pagination'
 import 'swiper/css/navigation'
 import GuestPreference from '@/components/PlaceList/GuestPreference'
 import './NavigationCustom.css'
-import like from '/public/images/like.svg'
+import emptyHeart from '/public/images/like.svg' // 회색하트
+import filledHeart from '/public/images/liked.svg' // 핑크하트
 import ArrowButton from '@/components/common/ArrowButton'
 import Image from 'next/image'
 import Link from 'next/link'
 
 // 장소 이미지 슬라이더 리스트 (사진 넘기는거)
 const PlaceCardImageList = ({ id, photos, guestFavorite }) => {
+  const [liked, setLiked] = useState(false)
+
+  const handleLikeClick = () => {
+    setLiked(!liked)
+  }
+
   return (
     <div className='relative w-full group'>
       <Swiper
@@ -31,7 +38,13 @@ const PlaceCardImageList = ({ id, photos, guestFavorite }) => {
             <div className='relative w-full h-full aspect-square'>
               {/* 하트 모양 표시 */}
               <div className='absolute top-2 right-2 z-20 p-2'>
-                <Image src={like} alt='Like' width={28} height={28} />
+                <Image
+                  src={liked ? filledHeart : emptyHeart}
+                  alt='Like'
+                  width={28}
+                  height={28}
+                  onClick={handleLikeClick}
+                />
               </div>
               {/* guestFavorite이 true면 게스트 선호 표시 */}
               {guestFavorite && (
