74052878c5923416dcae26b7a73b7cfb02b3c257
IforCU <tmdgus0844@gmail.com>
2024-06-02 15:30:48 +0900

    Fix: 철자 오류로 인해 data 반환이 안되는걸 고침

--- src/utils/sortByRating.js
+++ src/utils/sortByRating.js
@@ -1,9 +1,9 @@
 const sortByHighRatings = reviews => {
-  return [...reviews].sort((a, b) => b.ratings - a.ratings)
+  return [...reviews].sort((a, b) => b.rating - a.rating)
 }
 
 const sortByLowRatings = reviews => {
-  return [...reviews].sort((a, b) => a.ratings - b.ratings)
+  return [...reviews].sort((a, b) => a.rating - b.rating)
 }
 
 export { sortByHighRatings, sortByLowRatings }
