db0319e0826a9203af17736f0eb42b3de25797a7
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2024-06-03 21:27:10 +0900

    Design: main & rooms pageslayout 변경 했습니다~ 그리고 Rooms 페이지의 page.jsx 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import Header from '@/components/Header/Header'
-import Footer from '@/components/Footer/Footer'
-import Category from '@/components/MainCategory/Category'
 
 export const metadata: Metadata = {
   title: 'Airbnb Clone',
@@ -13,10 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='ko'>
       <body>
-        <div className='flex flex-col items-center transition-all lg:px-10 xl:px-20 sm:px-8 w-full'>
-          {children}
-        </div>
-        <Footer />
+        <div className='flex w-full flex-col items-center transition-all'>{children}</div>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Footer from '@/components/Footer/Footer'
 import Header from '@/components/Header/Header'
 import Category from '@/components/MainCategory/Category'
 import PlaceList from '@/components/PlaceList/PlaceList'
@@ -9,16 +10,19 @@ export default function Home({ searchParams }) {
   const categoryId = searchParams.category
 
   return (
-    <>
-      <div className='flex justify-center item-center w-full'>
-        <Header />
+    <div className='flex w-full flex-col items-center transition-all'>
+      <div className='sticky top-0 z-10 flex w-full flex-col bg-white'>
+        <div className='item-center flex w-full justify-center'>
+          <Header />
+        </div>
+        <div className='flex w-full items-center justify-center  overflow-auto sm:px-8 lg:px-10 xl:px-20'>
+          <Category />
+        </div>
       </div>
-      <div className='flex  justify-center items-center overflow-auto w-full'>
-        <Category />
-      </div>
-      <section className='w-full'>
+      <section className='h-[1200px] w-full overflow-auto sm:px-8 lg:px-10  xl:px-20'>
         <PlaceList categoryId={categoryId} />
       </section>
-    </>
+      <Footer />
+    </div>
   )
 }
