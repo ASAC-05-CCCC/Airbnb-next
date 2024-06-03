fadc2b6f40488794ab40bd621a646814d552ec59
yunhyungNAM <iys04003@naver.com>
2024-06-04 08:34:24 +0900

    Remove: test Consoles at SimpleInfo.jsx

--- src/components/RoomInfo/SimpleInfo.jsx
+++ src/components/RoomInfo/SimpleInfo.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 
-const SimpleInfo = () => {
+const SimpleInfo = ({ reviewData, reviewMetaData }) => {
   const [roomData, setRoomData] = useState(null);
   const pathname = usePathname();
   const id = pathname.slice(7);
@@ -14,13 +14,14 @@ const SimpleInfo = () => {
     .then(response => response.json())
     .then(data => {
       setRoomData(data)
-      console.log(data)
     })
     .catch(error => console.error('Error fetching room data:', error));
   }, []);
 
   if (!roomData) {
-    return <div>Loading...</div>;
+    return (
+      <></>
+    );
   }
   
   const briefInfo = roomData.briefRoomInfo.map((item, index) => `${item} ${index !== roomData.briefRoomInfo.length - 1 ? '· ' : ''}`);
@@ -30,7 +31,7 @@ const SimpleInfo = () => {
       <h1 className="text-2xl font-bold">{roomData.locationName}</h1>
       <p className="text-gray-700">{briefInfo}</p>
       <div className="flex items-center gap-1 pb-4">
-        <Image src="/images/star.svg" width={12} height={12} />
+        <Image src="/images/star.svg" alt='' width={12} height={12} />
         <span className="text-gray-700 font-bold">{roomData.rating}</span>
         <span className="text-gray-500 mx-2">·</span>
         <a href="#" className="text-gray-700 underline">후기 {roomData.reviewCount}개</a>
