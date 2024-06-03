fa922f89faca9adceaefa0e6b67677c83179250a
justlikesh <lpsac@naver.com>
2024-06-03 20:52:36 +0900

    Fix: 첫줄 공백수정

---
+++ src/components/Footer/FooterRooms.jsx
@@ -0,0 +1,15 @@
+import TravelRoadMap from '@/components/Footer/TravelRoadMap'
+import ExploreOtherOptionsTitle from '@/components/Footer/ExploreOtherOptionsTitle'
+import FooterTitleIn from '@/components/Footer/FooterTitleIn'
+
+const FooterRooms = () => {
+  return (
+    <footer>
+      <TravelRoadMap />
+      <ExploreOtherOptionsTitle />
+      <FooterTitleIn />
+    </footer>
+  )
+}
+
+export default FooterRooms
