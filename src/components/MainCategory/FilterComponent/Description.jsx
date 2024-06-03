86af52d24ecbee737c999cc512d5111afbb0f7b7
ji-mango <susie000301@naver.com>
2024-06-03 19:37:43 +0900

    Refactor: 반복적으로 사용되는 설명 텍스트를 Description 컴포넌트로 분리

---
+++ src/components/MainCategory/FilterComponent/Description.jsx
@@ -0,0 +1,3 @@
+export default function Description({ description }) {
+  return <div className='text-xs text-gray-500 font-normal mb-3'>{description}</div>
+}
--- src/components/MainCategory/FilterComponent/FilterEachComponent.jsx
+++ src/components/MainCategory/FilterComponent/FilterEachComponent.jsx
@@ -1,9 +1,11 @@
+import Description from '@/components/MainCategory/FilterComponent/Description'
+
 function FilterEachComponent({ title, description, children }) {
   return (
     <div className='flex flex-col m-6 h-auto border-b-[0.5px] border-b-gray-300 last:border-b-0'>
       <div className='mb-4'>
         <div className='text-xl font-medium mb-3'>{title}</div>
-        {description && <div className='text-xs text-slate-600 mb-3'>{description}</div>}
+        {description && <Description description={description} />}
       </div>
       {children}
     </div>
