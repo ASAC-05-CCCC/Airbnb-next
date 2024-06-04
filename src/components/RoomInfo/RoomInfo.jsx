bdfb7e9c8b6fcf33e771e2a15d916c4b4828ced8
yunhyungNAM <iys04003@naver.com>
2024-06-04 10:06:23 +0900

    feat: add new component at RoomInfo.jsx

--- src/components/RoomInfo/RoomInfo.jsx
+++ src/components/RoomInfo/RoomInfo.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import SimpleInfo from '@/components/RoomInfo/SimpleInfo.jsx'
 import RoomHost from '@/components/RoomInfo/RoomHost.jsx'
 import InfoDetail from '@/components/RoomInfo/InfoDetail.jsx'
+import GuestPrefer from './GuestPrefer'
 
 const RoomInfo = () => {
   return (
     <div>
       <SimpleInfo />
+      <GuestPrefer />
       <InfoDetail />
       <RoomHost />
     </div>
