45e78a149bd17ae0a6f8ccc174ad7fd87d7851bc
yunhyungNAM <iys04003@naver.com>
2024-06-04 10:05:36 +0900

    feat: add hiding feature at SimpleInfo.jsx according to json

--- src/components/RoomInfo/SimpleInfo.jsx
+++ src/components/RoomInfo/SimpleInfo.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 
-const SimpleInfo = ({ reviewData, reviewMetaData }) => {
+const SimpleInfo = () => {
   const [roomData, setRoomData] = useState(null);
   const pathname = usePathname();
   const id = pathname.slice(7);
@@ -27,15 +27,17 @@ const SimpleInfo = ({ reviewData, reviewMetaData }) => {
   const briefInfo = roomData.briefRoomInfo.map((item, index) => `${item} ${index !== roomData.briefRoomInfo.length - 1 ? '· ' : ''}`);
 
   return (
-    <div className="w-full grid gap-2">
+    <div className="w-full grid gap-2 pb-5">
       <h1 className="text-2xl font-bold">{roomData.locationName}</h1>
       <p className="text-gray-700">{briefInfo}</p>
-      <div className="flex items-center gap-1 pb-4">
-        <Image src="/images/star.svg" alt='' width={12} height={12} />
-        <span className="text-gray-700 font-bold">{roomData.rating}</span>
-        <span className="text-gray-500 mx-2">·</span>
-        <a href="#" className="text-gray-700 underline">후기 {roomData.reviewCount}개</a>
-      </div>
+      {!roomData.guestFavorite && (
+        <div className="flex items-center gap-1">
+          <Image src="/images/star.svg" alt='' width={12} height={12} />
+          <span className="text-gray-700 font-bold">{roomData.rating}</span>
+          <span className="text-gray-500 mx-2">·</span>
+          <a href="#" className="text-gray-700 underline">후기 {roomData.reviewCount}개</a>
+        </div>
+      )}
     </div>
   )
 }
