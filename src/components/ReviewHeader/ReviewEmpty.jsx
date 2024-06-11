db9440efe8dfe111e6f4b8ee422c5c9606ea3765
IforCU <tmdgus0844@gmail.com>
2024-06-11 13:03:45 +0900

    Feat : 슈퍼호스트가 아닐 때 컴포넌트 추가

---
+++ src/components/ReviewHeader/ReviewEmpty.jsx
@@ -0,0 +1,11 @@
+const ReviewEmpty = () => {
+  return (
+    <div className='w-full border-b-2 border-b-gray-200 py-12'>
+      <div className='text-start text-2xl font-bold'>
+        <span>후기 없음</span>
+      </div>
+    </div>
+  )
+}
+
+export default ReviewEmpty
