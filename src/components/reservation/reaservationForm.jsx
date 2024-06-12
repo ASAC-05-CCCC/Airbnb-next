84b18a0a9bb65e9ed793fed3f0ec94516a4a5a80
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2024-06-12 16:58:05 +0900

    Feat: rooms page 에약상태변경에 따라 searchParams 바뀌게 하기

--- src/components/reservation/reaservationForm.jsx
+++ src/components/reservation/reaservationForm.jsx
@@ -1,10 +1,32 @@
+'use client'
+
 import useModal from '@/hooks/useModal'
 import ReservationDateModalButton from './reservationDateModalButton'
 import ReservationGuestModalButton from '@/components/reservation/reservationGuestmodalButton'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { useSelector } from 'react-redux'
 
 export default function ReservationForm() {
   const { Modal: DateModal, open: dateOpen, close: dateClose, isOpen: dateIsOpen } = useModal()
   const { Modal: GuestModal, open: guestOpen, close: guestClose, isOpen: guestIsOpen } = useModal()
+
+  const { checkInDate, checkOutDate, guestCount } = useSelector(state => state.reservation)
+
+  const router = useRouter()
+
+  // 초기 url 값을 가져왔을 때, 해당 값을 읽어 예약 state에 업데이트 하는 초기 effect
+
+  // 모달을 눌렀을 때, url을 변경하는 effect
+  useEffect(() => {
+    const params = new URLSearchParams()
+    checkInDate && params.set('checkIn', checkInDate)
+    checkOutDate && params.set('checkOut', checkOutDate)
+
+    guestCount && params.set('guests', guestCount.adults)
+
+    router.push(`?${params.toString()}`, { scroll: false })
+  }, [checkInDate, checkOutDate, guestCount, router])
 
   return (
     <form onSubmit={e => e.preventDefault()}>
