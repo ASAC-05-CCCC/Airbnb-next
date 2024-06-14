138275a7aea20f12ac7c538826fec5662f1e33c0
yunhyungNAM <iys04003@naver.com>
2024-06-14 11:01:27 +0900

    Fix: Image tag CSS for stable rendering at RoomPictures.jsx

--- src/components/RoomView/RoomPictures.jsx
+++ src/components/RoomView/RoomPictures.jsx
@@ -27,12 +27,12 @@ const RoomPictures = () => {
   return (
     <div className="w-full transition-all">
       <div className="grid grid-cols-1 lg:grid-cols-4 gap-2 pb-4 w-full transition-all">
-        <div className="relative col-span-1 lg:col-span-2 row-span-1 lg:row-span-2  lg:aspect-auto transition-all h-[410px]" >
+        <div className="relative col-span-1 lg:col-span-2 row-span-1 lg:row-span-2 lg:aspect-auto transition-all h-[410px]" >
           <Image 
             src={roomData.imageUrl[0]} 
             alt="Large view"  
-            layout="fill"
-            objectFit="cover"
+            fill
+            sizes="(max-width: 1024px) 100vw, 50vw"
             className="rounded-lg rounded-tl-lg transition-all" 
           />
         </div>
@@ -40,8 +40,8 @@ const RoomPictures = () => {
           <Image 
             src={roomData.imageUrl[1]} 
             alt="View 1"
-            layout="fill"
-            objectFit="cover"
+            fill
+            sizes="(max-width: 1024px) 100vw, 25vw"
             className="rounded-lg transition-all" 
           />
         </div>
@@ -49,8 +49,8 @@ const RoomPictures = () => {
           <Image 
             src={roomData.imageUrl[2]} 
             alt="View 2"
-            layout="fill"
-            objectFit="cover"
+            fill
+            sizes="(max-width: 1024px) 100vw, 25vw"
             className="rounded-lg transition-all" 
           />
         </div>
@@ -58,8 +58,8 @@ const RoomPictures = () => {
           <Image 
             src={roomData.imageUrl[3]} 
             alt="View 3"
-            layout="fill"
-            objectFit="cover"
+            fill
+            sizes="(max-width: 1024px) 100vw, 25vw"
             className="rounded-lg transition-all" 
           />
         </div>
@@ -67,8 +67,8 @@ const RoomPictures = () => {
           <Image 
             src={roomData.imageUrl[4]} 
             alt="View 4"
-            layout="fill"
-            objectFit="cover"
+            fill
+            sizes="(max-width: 1024px) 100vw, 25vw"
             className="rounded-lg transition-all" 
           />
           <button className="absolute bottom-5 right-5 flex items-center justify-center bg-white text-lg rounded-lg border border-black px-4 py-0.5 transition-all">
