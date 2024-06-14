32b972d27646d05efc473f83727ea26d0cc15fee
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2024-06-15 05:50:59 +0900

    Design: 반응형 CSS 수정 및 미완성 내용 주석 처리

--- src/components/Header/SearchBar/searchFormWrapper.jsx
+++ src/components/Header/SearchBar/searchFormWrapper.jsx
@@ -12,9 +12,9 @@ function SearchFormWrapper({ searchBarOpen }) {
       ></div>
       <div
         className={clsx(
-          ' left-0 top-8 z-20 mb-2 flex w-full justify-center lg:absolute lg:px-32',
-          'transition-all duration-300 before:absolute before:left-0 before:h-full before:w-full',
-          !searchBarOpen && '-translate-y-20 scale-0',
+          'left-0 top-8 z-20 mb-2 flex h-full w-full justify-center transition-all duration-300 lg:absolute lg:px-32',
+          // 'before:absolute before:left-0 before:top-2/4 before:h-screen before:w-full before:bg-[#00000040]',
+          !searchBarOpen && '-translate-y-28 scale-0 opacity-0 sm:h-0',
         )}
       >
         <SearchForm />
