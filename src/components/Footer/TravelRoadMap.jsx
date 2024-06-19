b199fac04ce152a58b6acbd8ade948a9de9b78d2
justlikesh <lpsac@naver.com>
2024-06-19 12:13:46 +0900

    Fix : TravelRoadMap의 버튼을 api로 연결

--- src/components/Footer/TravelRoadMap.jsx
+++ src/components/Footer/TravelRoadMap.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
+'use client'
+import { usePathname } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
 
-const TravelRoadMap = () => {
-  const paths = ['A', 'B', 'C', 'D']
+function TravelRoadMap() {
+  const [paths, setPath] = useState([])
+  const pathname = usePathname() //URL path를 가져옴 ex) /rooms/5
+  const id = pathname.slice(7) // slice(7) -> 문자열 7번쨰부터
+
+  useEffect(() => {
+    fetch(`/apis/footer/${id}`)
+      .then(response => response.json())
+      .then(data => {
+        setPath(['에어비앤비', data.region_name_1, data.region_name_2, data.city])
+      })
+      .catch(error => console.error('Error fetching host.json:', error))
+  }, [])
 
   return (
-    <section className='bg-gray-100  text-gray-700 py-0 px-2 sm:px-6 lg:px-8  '>
-      <div className='max-w-7xl mx-auto px-6 lg:px-8'>
+    <section className='bg-gray-100  px-2 py-0 text-gray-700 sm:px-6 lg:px-8  '>
+      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
         {/* <div className='mx-auto px-4 sm:px-6 lg:px-8'> */}
-        <div className='flex items-center space-x-2 text-gray-600 py-8 text-sm font-light'>
+        <div className='flex items-center space-x-2 py-8 text-sm font-light text-gray-600'>
           {paths.map((path, index) => (
             <React.Fragment key={index}>
-              <button className='text-gray-600 hover:text-blue-600 focus:outline-none hover:underline'>
+              <button className='text-gray-600 hover:text-blue-600 hover:underline focus:outline-none'>
                 {path}
               </button>
               {index < paths.length - 1 && <span className='mx-1'>{'>'}</span>}
             </React.Fragment>
           ))}
         </div>
-        <hr className='border-gray-300 mb-0 mt-0 w-screen relative left-1/2 transform -translate-x-1/2 border-t-3' />
+        <hr className='border-t-3 relative left-1/2 mb-0 mt-0 w-screen -translate-x-1/2 transform border-gray-300' />
       </div>
 
       {/* </div> */}
